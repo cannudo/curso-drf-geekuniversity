0e2166ac76f318954168898b51ece34c42e37d50
Luan C. Redmann <luandacostaredmann@gmail.com>
2022-04-09 21:09:59 -0300

    🛂 aponta as classes de autenticação e permissão

--- escola/settings.py
+++ escola/settings.py
@@ -126,3 +126,12 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': {
+        'rest_framework.authentication.SessionAuthentication'
+    },
+    'DEFAULT_PERMISSION_CLASSES': {
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    }
+}
