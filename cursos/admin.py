4ed6839e5242f141fe45879ae3f3a26730b95f62
Luan C. Redmann <luandacostaredmann@gmail.com>
2022-04-09 21:00:09 -0300

    🚀 configuração inicial

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Curso, Avaliacao
 # Register your models here.
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo')
+
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('curso', 'nome', 'email', 'avaliacao', 'criacao', 'atualizacao', 'ativo')
