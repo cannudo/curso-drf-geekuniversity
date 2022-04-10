3d4668d64171fbf62b7abec6c2aba1668df8f1a1
Luan C. Redmann <luandacostaredmann@gmail.com>
2022-04-09 22:19:13 -0300

    ✨ disponibiliza serializers

---
+++ cursos/serializers.py
@@ -0,0 +1,45 @@
+'''
+Um serializer pega um dado (ou uma estrutura de dados) Python
+e o formata em JSON. O contrário também é verdadeiro.
+'''
+
+from rest_framework import serializers
+
+from .models import Curso, Avaliacao
+
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = {
+            'email': {
+                'write_only': True # Só pega o e-mail na hora de gravar o registro
+            }
+        }
+        model = Avaliacao
+        fields = (
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo',
+        )
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    '''
+    Uma instância do serializer retorna os campos do model
+    bem como seus respectivos tipos.
+    '''
+    class Meta:
+        model = Curso
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo',
+        )
