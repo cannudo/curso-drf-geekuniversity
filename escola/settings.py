804b8cbd37ab8f5cd2bd58465027478e8fc441d1
Luan C. Redmann <luandacostaredmann@gmail.com>
2022-04-12 21:16:05 -0300

    📦️ coloca paginação na request

--- escola/settings.py
+++ escola/settings.py
@@ -133,5 +133,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
