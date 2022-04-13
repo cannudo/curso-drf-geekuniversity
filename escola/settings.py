69f277d76cf1f829e2132159778fde8e0bc1f230
Luan C. Redmann <luandacostaredmann@gmail.com>
2022-04-12 22:06:02 -0300

    🛂 muda método de autenticação para token

--- escola/settings.py
+++ escola/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'django_filters',
     'rest_framework',
+    'rest_framework.authtoken',
 
     'cursos',
 ]
@@ -129,7 +130,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
--- escola/urls.py
+++ escola/urls.py
@@ -21,6 +21,5 @@ from cursos.urls import router
 urlpatterns = [
     path('api/v1/', include('cursos.urls')),
     path('api/v2/', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls'))
+    path('admin/', admin.site.urls)
 ]
